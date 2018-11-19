44cfd8c49f07035d2a9804f78205c08583aa1f25
duanwandao <1097576492@qq.com>
2018-11-19 10:49:49 +0800

    品优购第二天完成

---
+++ pinyougou-pojo/src/main/java/com/pinyougou/entity/Result.java
@@ -0,0 +1,31 @@
+package com.pinyougou.entity;
+
+/**
+ * 返回结果
+ * @author Administrator
+ *
+ */
+public class Result {
+ 
+	private boolean success;  //是否成功
+	private String message;   //返回信息
+	public Result(boolean success, String message) {
+		super();
+		this.success = success;
+		this.message = message;
+	}
+	public boolean isSuccess() {
+		return success;
+	}
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
